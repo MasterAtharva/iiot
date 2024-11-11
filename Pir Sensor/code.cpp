84858b68bceca51537800847c1e7ed65ee3062d8
Vedpathak Atharva Maheshkumar <133250394+MasterAtharva@users.noreply.github.com>
2024-11-11 22:40:27 +0530

    Add files via upload

---
+++ Pir Sensor/code.cpp
@@ -0,0 +1,22 @@
+int led = 11; // pin where LED is connected
+int pir_sensor = 1; // pin where PIR sensor is connected
+
+void setup()
+{
+  pinMode(led, OUTPUT); // set LED pin for output
+  pinMode(pir_sensor, INPUT); // set PIR sensor pin for reading input
+  digitalWrite(led, LOW); // keep LED off initially
+}
+
+void loop()
+{
+  int sensor_val = digitalRead(pir_sensor); // read PIR sensor value
+  if (sensor_val == HIGH) // if motion is detected
+  {
+    digitalWrite(led, HIGH); // turn LED on
+  }
+  else // if no motion is detected
+  {
+    digitalWrite(led, LOW); // turn LED off
+  }
+}
